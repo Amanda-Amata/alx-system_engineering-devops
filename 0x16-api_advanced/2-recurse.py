0fc2a6e1aa0a8a85ca3fadc8a6248c0e11bf4dd5
Amanda Mabunda <amandamabu14@gmail.com>
2024-05-07 17:10:36 +0200

    reddit api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API and 
-	returns a list containing the titles of all hot articles"""
+"""recursive function that queries the Reddit API and
+    returns a list containing the titles of all hot articles
+"""
 import requests
 from sys import argv
 
+
 def recurse(subreddit, hot_list=[], after="", count=0):
-	"""returns a list of all titles of hot articles"""
-	url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """returns a list of all titles of hot articles"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "google/5.0"}
     params = {
-        "after": after,
-        "count": count,
-        "limit": 100
+            "after": after,
+            "count": count,
+            "limit": 100
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
