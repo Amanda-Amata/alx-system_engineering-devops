a190ed591817af3e6371ee44d4589798da710363
Amanda Mabunda <amandamabu14@gmail.com>
2024-05-07 15:24:12 +0200

    all tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that queries the Reddit API and returns the number of subscribers """
+import requests
+
+def number_of_subscribers(subreddit):
+	""" returns number of subscribers"""
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {"user-Agent": "google/1.0"}
+	response = request.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 200:
+		data = response.json()
+		subscribers = data['data']['subscribers']
+		return subscribers
+	else:
+		return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that queries the Reddit API and 
+	prints the titles of the first 10 subreddit"""
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+	""" returns the first 10 hot posts"""
+	user = {'User-Agent': 'baby reindeer'}
+	url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+	try:
+		for post in url.get('data').get('children'):
+			print(post.get('data).get('title'))
+	except Exception:
+		print(None)
+
+
+if __name__ == "__main__":
+	top_ten(argv[1])
